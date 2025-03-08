2dd74b64ea61aa3ca45c5dfa0dc17e0507b1a200
Y2ktorrez <andrestorrezvaca@gmail.com>
2025-03-08 13:58:19 -0400

    Update changes

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>Hola Mundo</h1>
+      <h1  className={titleFont.className}>Hola Mundo2</h1>
+    </div>
+  );
+}
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>LoginPage</h1>
+    </div>
+  );
+}
